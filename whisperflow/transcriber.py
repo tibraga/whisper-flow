9325b9c12675b58553e9024522f6bae3c6237865
Tiago Braga <tiagobraga@1sti.com.br>
2024-11-27 11:40:52 -0300

    Adjust

--- whisperflow/transcriber.py
+++ whisperflow/transcriber.py
@@ -46,7 +46,7 @@ def transcribe_pcm_chunks(
     # Remover silêncio
     trimmed_arr, index = librosa.effects.trim(
         arr,
-        top_db=20,
+        top_db=10,
         frame_length=frame_length,
         hop_length=hop_length
     )
